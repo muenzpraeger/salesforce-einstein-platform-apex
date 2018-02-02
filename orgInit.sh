9c717b28564293abdd70332c569523bc6c40ca00
muenzpraeger <rwinkelmeyer@salesforce.com>
2018-02-02 09:30:17 +0100

    Final cleanup for v3

--- orgInit.sh
+++ orgInit.sh
@@ -3,6 +3,5 @@ sfdx force:org:create -s -f config/project-scratch-def.json -d 1
 sfdx force:source:push
 # custom settings setup.  Modify for your username
 sfdx force:user:permset:assign -n Einstein_Platform_Playground
-sfdx force:data:record:create -s Einstein_Settings__c -v "Einstein_EMail__c='your.user@email.com' CertName__c=key2einstein"
 
 sfdx force:org:open -p /one/one.app#/setup/CertificatesAndKeysManagement/home
